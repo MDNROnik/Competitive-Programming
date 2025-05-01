b6bc45d43f40313e20d1608eea41b9c0dcd59b74
MDNROnik <nimurrahnananik@gmail.com>
2025-05-01 10:02:08 +0600

    new problem solution

---
+++ Codeforces/A. Make It Zero.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void Yes(int check){
+    if(check){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"Yes"<<endl;
+    }
+}
+void No(int check){
+    if(check){
+        cout<<"NO"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;cin>>t;
+    while(t--){
+        ll n;cin>>n;
+        for(int i=0;i<n;i++){
+            ll a;cin>>a;
+        }
+        if(n%2==0){
+            cout<<2<<endl;
+            cout<<1<<" "<<n<<endl;
+            cout<<1<<" "<<n<<endl;
+        }
+        else{
+            cout<<4<<endl;
+            cout<<1<<" "<<n-1<<endl;
+            cout<<1<<" "<<n-1<<endl;
+            cout<<n-1<<" "<<n<<endl;
+            cout<<n-1<<" "<<n<<endl;
+        }
+    }
+
+    return 0;
+}
+
+
