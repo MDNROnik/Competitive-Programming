ca496c559b9dd28265b8555e2e268be9ac1bb85c
MDNROnik <nimurrahnananik@gmail.com>
2025-05-17 18:07:07 +0600

    add new 1000 rating problem solution

---
+++ Codeforces/B. Bogosort.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+
+#include <numeric>
+#define ll long long int
+using namespace std;
+bool prime[90000001];
+int n=90000000;
+vector<int>v = {1};
+
+void sieve()
+{
+	for(int i=2;i*i<=n;i++)
+	{
+		if(prime[i]==false)
+		{
+			for(int j=i*i;j<=n;j+=i)
+			{
+				prime[j]=true;
+			}
+		}
+	}
+	for(int i=2;i<=n;i++)
+	{
+		if(prime[i]==false)v.push_back(i);
+	}
+}
+
+void Yes(int check){
+    if(check){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"Yes"<<endl;
+    }
+}
+void No(int check){
+    if(check){
+        cout<<"NO"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+}
+ll gcd(ll a, ll b) {
+    while(b != 0) {
+        ll temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+ll lcm(ll a, ll b) {
+    return (a / gcd(a, b)) * b;
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //sieve();
+    ll t;cin>>t;
+    while(t--){
+        ll n;cin>>n;
+        vector<ll>v;
+        for(int i=0;i<n;i++){
+            ll a;cin>>a;
+            v.push_back(a);
+        }
+        sort(v.begin(),v.end());
+        reverse(v.begin(), v.end());
+        for(ll i=0;i<n;i++){
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
